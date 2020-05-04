b2fa618ed3f371fb80a956390284c4a935b22b3e
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-05 06:56:34 +0700

    #16 - Add OAuth2AuthorizationServerConfig

---
+++ src/main/java/com/hendisantika/springbootoauth2demo/config/OAuth2AuthorizationServerConfig.java
@@ -0,0 +1,38 @@
+package com.hendisantika.springbootoauth2demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-oauth2-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/05/20
+ * Time: 06.56
+ */
+@Configuration
+@EnableAuthorizationServer
+@Import(WebSecurityConfig.class)
+public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    @Qualifier("dataSource")
+    private DataSource dataSource;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+}
