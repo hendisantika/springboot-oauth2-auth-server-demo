edd7cf5428a817770b8a92e4cb6cb8fdd9139d3c
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-05 06:54:22 +0700

    #14 - Add UserDetailsServiceImpl

---
+++ src/main/java/com/hendisantika/springbootoauth2demo/service/UserDetailsServiceImpl.java
@@ -0,0 +1,52 @@
+package com.hendisantika.springbootoauth2demo.service;
+
+import com.hendisantika.springbootoauth2demo.dto.CustomGrantedAuthority;
+import com.hendisantika.springbootoauth2demo.dto.CustomUserDetails;
+import com.hendisantika.springbootoauth2demo.model.User;
+import com.hendisantika.springbootoauth2demo.model.UserAuthority;
+import com.hendisantika.springbootoauth2demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-oauth2-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/05/20
+ * Time: 06.53
+ */
+@Service
+@Transactional
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user != null) {
+            CustomUserDetails customUserDetails = new CustomUserDetails();
+            customUserDetails.setUserName(user.getUserName());
+            customUserDetails.setPassword(user.getPassword());
+            Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+            for (UserAuthority authority : user.getUserAuthorities()) {
+                authorities.add(new CustomGrantedAuthority(authority.getAuthority().getName()));
+            }
+            customUserDetails.setGrantedAuthorities(authorities);
+            return customUserDetails;
+        }
+        throw new UsernameNotFoundException(username);
+    }
+
+}
