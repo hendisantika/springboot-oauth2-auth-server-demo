6d92eb3640ef2771205f950db4244e9829849db8
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-05 06:35:05 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootoauth2demo/SpringbootOauth2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootoauth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOauth2DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOauth2DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootoauth2demo/SpringbootOauth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootoauth2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOauth2DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
